5ae240744b4ac7e8a39b6713b449881eeb220313
sebaszap123-dev <freeway.dev.sf@gmail.com>
2023-07-27 16:30:36 -0600

    solved error with email register

--- lib/widgets/form_sign_up.dart
+++ lib/widgets/form_sign_up.dart
@@ -74,11 +74,20 @@ class _FormSignUpState extends State<FormSignUp> {
               onPressed: () async {
                 // TODO: Implement signUp
                 if (!isValidForm()) return;
-                final response = await FirebaseAuth.instance
-                    .createUserWithEmailAndPassword(
-                        email: email.text, password: password.text);
-                print(response);
-                context.router.pushNamed('sign_in');
+                try {
+                  UserCredential userCredential = await FirebaseAuth.instance
+                      .createUserWithEmailAndPassword(
+                          email: email.text.trim(),
+                          password: password.text.trim());
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'weak-password') {
+                    print('The password provided is too weak.');
+                  } else if (e.code == 'email-already-in-use') {
+                    print('The account already exists for that email.');
+                  }
+                } catch (e) {
+                  print(e);
+                }
               },
               child: Text(
                 'Picale we',
